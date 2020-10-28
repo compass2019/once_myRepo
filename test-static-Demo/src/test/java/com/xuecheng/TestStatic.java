43b30d137b5b923ece0cc70796f9d87fd2576cee
compass <751191428@qq.com>
2020-10-29 00:28:43 +0800

    提交test-static-Demo

--- test-static-Demo/src/test/java/com/xuecheng/TestStatic.java
+++ test-static-Demo/src/test/java/com/xuecheng/TestStatic.java
@@ -1,17 +1,34 @@
 package com.xuecheng;
 
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootApplication
 @RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class TestStatic {
 
 
+//    static final Integer a;
 
+    public TestStatic() {
+//        a = 1;
+    }
 
+    //    static Integer b;
+//
+//    static {
+//        a = 4;
+//    }
+    @Test
+    public void ss() {
+//        System.out.println(a);
+    }
 
 
 }
+
+
+
