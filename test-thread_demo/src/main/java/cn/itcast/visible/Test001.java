4e835104feffcfbe188744a4b2d10eeeb111ee9c
compass <751191428@qq.com>
2020-10-29 00:27:45 +0800

    提交test-thread_demo

--- test-thread_demo/src/main/java/cn/itcast/visible/Test001.java
+++ test-thread_demo/src/main/java/cn/itcast/visible/Test001.java
@@ -3,10 +3,14 @@ package cn.itcast.visible;
 public class Test001 {
     public static void main(String[] args) {
         String s1 = new String("abc");
-        String s2 = new String("def");
-        String s3 = s1.intern() + s2.intern();
-        String s4 = "abcdef";
-        System.out.println(s3 == s4);
+//        String s2 = new String("def");
+//        String s3 = s1.intern() + s2.intern();
+//        String s4 = "abcdef";
+//        System.out.println(s3 == s4);
+        String s5 = "abc";
+        String s6 = s1.intern();
+        System.out.println(s1.equals(s5));//false
+        System.out.println(s5==s6);//true
 
     }
 }
